0a298834ea054b95a3efbb2efe2ac9549331dd92
Kacper <kucza06@wp.pl>
2021-02-15 17:01:47 +0100

    add ThemeProvider

--- src/root/App/App.js
+++ src/root/App/App.js
@@ -1,13 +1,16 @@
+import React from 'react'
 import './App.css';
+import { ThemeProvider } from 'styled-components'
 import { BrowserRouter } from "react-router-dom";
 import Router from "root/Router";
+import theme from 'constants/theme';
 
-const App = () => {
-  return (
+const App = () => (
+  < ThemeProvider theme={theme} >
     <BrowserRouter>
       <Router />
     </BrowserRouter>
-  );
-}
+  </  ThemeProvider>
+);
 
 export default App;
