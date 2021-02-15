62f9a1295bed2b06fc4cf6145e624a30104254f9
Kacper <kucza06@wp.pl>
2021-02-15 15:10:55 +0100

    add Category component

---
+++ src/components/Category/Category.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Category = () => {
+    return (
+        <div>
+            <h1>
+                Kategoria
+            </h1>
+        </div>
+    )
+}
+
+export default Category
--- src/root/App/App.js
+++ src/root/App/App.js
@@ -1,11 +1,15 @@
+import React from 'react'
 import './App.css';
 import { BrowserRouter } from "react-router-dom";
 import Router from "root/Router";
+
+import Category from '../../components/Category'
 
 const App = () => {
   return (
     <BrowserRouter>
       <Router />
+      <Category />
     </BrowserRouter>
   );
 }
