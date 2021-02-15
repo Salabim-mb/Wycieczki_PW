4eead7f3c41bd3ea6f2ae0facc3a2f83be3773a8
adaxelx <adaxelx@o2.pl>
2021-02-15 17:19:54 +0100

    install RTL and fix app test

---
+++ src/root/App/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
+import Router from 'root/Router';
+
+const App = () => (
+	<BrowserRouter>
+		<Router />
+	</BrowserRouter>
+);
+
+export default App;
