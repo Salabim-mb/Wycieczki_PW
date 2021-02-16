0cd7220b28c3bab13edce987ca9584d9f61677c7
Kacper <kucza06@wp.pl>
2021-02-16 16:59:30 +0100

    add Container and some testing data

--- src/components/Category/Category.jsx
+++ src/components/Category/Category.jsx
@@ -2,24 +2,49 @@ import React from 'react';
 import Card from '@material-ui/core/Card';
 import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
+import Container from '@material-ui/core/Container';
 import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
 
+import Image from './components/Image'
+
+import logo from '../../logo.svg'
+
+const dummyData = [
+    {
+        title: 'XDD',
+        date: '21.21.2002'
+    },
+    {
+        title: 'XDD',
+        date: '21.21.2002'
+    },
+    {
+        title: 'XDD',
+        date: '21.21.2002'
+    },
+]
 
 const Category = () => (
-    <Card >
-        <CardContent>
-            <Typography color="textSecondary" gutterBottom>
-                21.11.2000
-        </Typography>
-            <Typography variant="h5" component="h2">
-                Tytuł wpisu
-            </Typography>
-        </CardContent>
-        <CardActions>
-            <Button size="small">Learn More</Button>
-        </CardActions>
-    </Card>
+    <Container>
+        <Image imgSrc={logo} imgAlt="obrazek" />
+        {dummyData.map((data) => (
+            <Card >
+                <CardContent>
+                    <Typography color="textSecondary" gutterBottom>
+                        {data.date}
+                    </Typography>
+                    <Typography variant="h5" component="h2">
+                        {data.tiitle}
+                    </Typography>
+                </CardContent>
+                <CardActions>
+                    <Button size="small">Learn More</Button>
+                </CardActions>
+            </Card>
+        ))}
+
+    </Container>
 );
 
 export default Category
---
+++ src/components/Category/components/Image.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const Image = ({ imgSrc, imgAlt }) => (
+    <img src={imgSrc} alt={imgAlt} />
+)
+
+export default Image
