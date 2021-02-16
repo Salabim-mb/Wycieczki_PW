c568b546f76d5a0302ccd182f14c5d3e2e51e2e2
adaxelx <adaxelx@o2.pl>
2021-02-16 13:57:34 +0100

    install CKEditor

--- src/views/PostCreator/PostCreator.jsx
+++ src/views/PostCreator/PostCreator.jsx
@@ -1,5 +1,30 @@
 import React from 'react';
+import { CKEditor } from '@ckeditor/ckeditor5-react';
+import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
 
-const PostCreator = () => <></>;
+const PostCreator = () => {
+	function MyCustomUploadAdapterPlugin(editor) {
+		// adapter for images
+		return editor;
+	}
+
+	return (
+		<>
+			{' '}
+			<CKEditor
+				config={{
+					language: 'pl',
+					extraPlugins: [MyCustomUploadAdapterPlugin],
+				}}
+				onChange={(e, editor) => {
+					console.log(editor, e);
+				}}
+				className="text-justify"
+				editor={ClassicEditor}
+				data=""
+			/>
+		</>
+	);
+};
 
 export default PostCreator;
