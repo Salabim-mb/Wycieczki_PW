0de556325f4fc6bb34a5817157af270657ce120c
adaxelx <adaxelx@o2.pl>
2021-02-17 00:37:16 +0100

    add some inputs to PostForm

--- src/views/PostCreator/PostCreator.jsx
+++ src/views/PostCreator/PostCreator.jsx
@@ -1,40 +1,25 @@
 import React from 'react';
-import { CKEditor } from '@ckeditor/ckeditor5-react';
-import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
 import { useQuery } from 'react-query';
 import { AlertInfo } from 'components';
+import { Container, Card, CardHeader, CardContent } from '@material-ui/core';
 import { getPost } from './PostCreator.api';
 import { PostForm } from './components';
 
 const PostCreator = () => {
-	function MyCustomUploadAdapterPlugin(editor) {
-		// adapter for images
-		return editor;
-	}
-
 	const { status, isLoading, isError, error, data } = useQuery('post', getPost);
 
-	console.log(status);
-
 	return (
-		<>
-			<PostForm post={data} />
-			<CKEditor
-				config={{
-					language: 'pl',
-					extraPlugins: [MyCustomUploadAdapterPlugin],
-				}}
-				onChange={(e, editor) => {
-					console.log(editor, e);
-				}}
-				className="text-justify"
-				editor={ClassicEditor}
-				data=""
-			/>
-			<AlertInfo isLoading={isLoading} isError={isError}>
-				{error?.message}
-			</AlertInfo>
-		</>
+		<Container>
+			<Card>
+				<CardHeader title="Stwórz wpis" />
+				<CardContent>
+					<PostForm post={data} />{' '}
+					<AlertInfo isLoading={isLoading} isError={isError}>
+						{error?.message}
+					</AlertInfo>
+				</CardContent>
+			</Card>
+		</Container>
 	);
 };
 
