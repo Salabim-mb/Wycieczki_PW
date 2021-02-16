b60a42eb774d7bac61d73ce93143fdf14a80aba0
adaxelx <adaxelx@o2.pl>
2021-02-17 00:37:37 +0100

    add some inputs to PostForm

--- src/views/PostCreator/components/PostForm/PostForm.jsx
+++ src/views/PostCreator/components/PostForm/PostForm.jsx
@@ -1,41 +1,98 @@
-import React, { useState } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import { useMutation } from 'react-query';
 import { sendPost } from 'views/PostCreator/PostCreator.api';
+import { CKEditor } from '@ckeditor/ckeditor5-react';
+import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
+import { TextField, Button } from '@material-ui/core';
+import { useFormik } from 'formik';
+import { CloudUpload } from '@material-ui/icons';
 
 const PostForm = ({ post }) => {
-	const [header, setHeader] = useState(post?.header || null);
-	const [cover, setCover] = useState(post?.cover || null);
-	const [title, setTitle] = useState(post?.title || '');
-	const [content, setContent] = useState(post?.content || '');
-
 	const mutation = useMutation(postData => sendPost('', postData));
+
+	function MyCustomUploadAdapterPlugin(editor) {
+		// adapter for images
+		return editor;
+	}
+
+	const formik = useFormik({
+		initialValues: {
+			header: post?.header || null,
+			cover: post?.cover || '',
+			title: post?.title || '',
+			content: post?.content || '',
+		},
+
+		onSubmit: values => {
+			mutation.mutate(values);
+		},
+	});
 
 	return (
 		<>
-			<button
-				type="submit"
-				onClick={() => {
-					mutation.mutate({ header, cover, title, content });
-				}}
-			>
-				Create Todo
-			</button>
+			<form onSubmit={formik.handleSubmit}>
+				<Button variant="contained" component="label" color="default" startIcon={<CloudUpload />}>
+					Prześlij cover
+					<input
+						id="cover"
+						name="cover"
+						type="file"
+						hidden
+						onChange={event => {
+							formik.setFieldValue('cover', event.currentTarget.files[0]);
+						}}
+					/>
+				</Button>
+				<Button variant="contained" component="label" color="default" startIcon={<CloudUpload />}>
+					Prześlij nagłówek
+					<input
+						id="header"
+						name="header"
+						type="file"
+						hidden
+						onChange={event => {
+							formik.setFieldValue('header', event.currentTarget.files[0]);
+						}}
+					/>
+				</Button>
+				<TextField
+					fullWidth
+					id="title"
+					name="title"
+					label="Tytuł posta"
+					value={formik.values.title}
+					onChange={formik.handleChange}
+				/>
+				<CKEditor
+					config={{
+						language: 'pl',
+						extraPlugins: [MyCustomUploadAdapterPlugin],
+					}}
+					onChange={(e, editor) => {
+						console.log(editor, e);
+					}}
+					className="text-justify"
+					editor={ClassicEditor}
+					data=""
+				/>
+				<Button type="submit">Prześlij post</Button>
+			</form>
 		</>
 	);
 };
 
 PostForm.propTypes = {
 	post: PropTypes.shape({
-		header: PropTypes.element.isRequired,
-		cover: PropTypes.element.isRequired,
+		header: PropTypes.string.isRequired,
+		cover: PropTypes.string.isRequired,
 		title: PropTypes.string.isRequired,
 		content: PropTypes.string.isRequired,
 	}),
 };
 
 PostForm.defaultProps = {
-	post: undefined,
+	post: null,
 };
 
 export default PostForm;
