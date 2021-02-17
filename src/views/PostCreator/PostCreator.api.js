4bb9cee4b5bf01397ea8bf4a571c801e2adc8ad6
adaxelx <adaxelx@o2.pl>
2021-02-18 00:12:10 +0100

    add post creator requests

--- src/views/PostCreator/PostCreator.api.js
+++ src/views/PostCreator/PostCreator.api.js
@@ -1,7 +1,9 @@
+import paths from 'constants/api';
+
 export const getPost = async (token, id) => {
 	if (id) {
 		// fetch post
-		const url = '';
+		const url = `${paths.BLOG_POST}${id}/`;
 
 		const headers = {};
 
@@ -17,37 +19,30 @@ export const getPost = async (token, id) => {
 };
 
 export const sendPost = async (token, post, id) => {
-	const url = `${id}`;
+	const url = `${paths.BLOG_POST}${id ? `${id}/` : ''}`;
 
 	const headers = {
 		Authorization: `${token}`,
 	};
 
-	const response = await fetch(url, { headers, method: 'GET' });
+	const method = id ? 'PATCH' : 'POST';
+	const expectedResponseStatus = id ? 200 : 201;
 
-	if (response.status === 201) {
-		return response.json();
+	const response = await fetch(url, { headers, method });
+
+	if (response.status !== expectedResponseStatus) {
+		throw new Error('');
 	}
-	throw new Error('');
 };
 
-export const uploadPhoto = async (token, post, id) => {
-	const url = `${id}`;
+export const uploadPhoto = async (token, id, file, flag, attachment) => {
+	const url = `${paths.BLOG_RESERVATION}${id}/${attachment ? 'attachment' : 'image'}/`;
 
-	const headers = {
-		Authorization: `${token}`,
-	};
-
-	const response = await fetch(url, { headers, method: 'GET' });
-
-	if (response.status === 201) {
-		return response.json();
+	const formData = new FormData();
+	formData.append('file', file, file.name);
+	if (flag) {
+		formData.append(flag, true);
 	}
-	throw new Error('');
-};
-
-export const reserveSpace = async (token, post, id) => {
-	const url = `${id}`;
 
 	const headers = {
 		Authorization: `${token}`,
@@ -59,4 +54,54 @@ export const reserveSpace = async (token, post, id) => {
 		return response.json();
 	}
 	throw new Error('');
+};
+
+export const uploadAttachment = async (token, id, file) => uploadPhoto(token, id, file, null, true);
+
+export const deleteAttachment = async (token, id, isImage) => {
+	const startOfUrl = isImage ? paths.BLOG_IMAGE : paths.BLOG_DOWNLOADABLE;
+	const url = `${startOfUrl}${id}/`;
+
+	const headers = {
+		Authorization: `${token}`,
+	};
+
+	const response = await fetch(url, { headers, method: 'DELETE' });
+
+	if (response.status === 200) {
+		return response.json();
+	}
+	throw new Error('');
+};
+
+export const reserveSpace = async token => {
+	const url = `${paths.BLOG_RESERVATION}`;
+
+	const headers = {
+		Authorization: `${token}`,
+	};
+
+	const response = await fetch(url, { headers, method: 'POST' });
+
+	if (response.status === 201) {
+		return response.json();
+	}
+	throw new Error('');
+};
+
+export const getReservation = async id => {
+	if (id) {
+		const url = `${paths.BLOG_RESERVATION}${id}/`;
+
+		const headers = {};
+
+		const response = await fetch(url, { headers, method: 'GET' });
+
+		if (response.status === 200) {
+			return response.json();
+		}
+		throw new Error('');
+	}
+	// return empty if id is not provided
+	return {};
 };
