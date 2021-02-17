150ed0ae5bae29ef06b8b47ccdaf751d5b4dba4b
adaxelx <adaxelx@o2.pl>
2021-02-17 23:46:07 +0100

    add constants api routes

---
+++ src/constants/api.js
@@ -0,0 +1,22 @@
+const domain = process.env.REACT_APP_BACKEND_PATH;
+
+const BLOG = `${domain}/blog/`;
+const BLOG_TOPIC = `${BLOG}topic/`;
+const BLOG_RESERVATION = `${BLOG}post-reservation/`;
+const BLOG_POST = `${BLOG}post/`;
+
+const paths = {
+	BLOG,
+	BLOGS: `${domain}/blogs/`,
+	BLOG_TOPIC,
+	BLOG_TOPICS: `${BLOG}topics/`,
+	BLOG_RESERVATION,
+	BLOG_POST,
+	BLOG_POSTS: `${BLOG}posts/`,
+	BLOG_INDEX_CHANGE: `${BLOG}index-change/`,
+	TOPIC_INDEX_CHANGE: `${BLOG_TOPIC}index-change/`,
+	BLOG_IMAGE: `${BLOG}image-attachment/`,
+	BLOG_DOWNLOADABLE: `${BLOG}downloadable-attachment/`,
+};
+
+export default paths;
