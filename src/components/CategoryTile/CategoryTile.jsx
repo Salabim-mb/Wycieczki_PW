3ff5560060268cb66694c46db2e6d4f177c38be6
Kacper <kucza06@wp.pl>
2021-02-18 22:17:38 +0100

    chagne static view to view with props

--- src/components/CategoryTile/CategoryTile.jsx
+++ src/components/CategoryTile/CategoryTile.jsx
@@ -8,11 +8,10 @@ import CardMedia from '@material-ui/core/CardMedia';
 import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
 
-import logo from '../../logo.svg'
+import { Link } from 'react-router-dom'
 
 const StyledCardMedia = styled(CardMedia)`
     height: 200px;
-
 
     @media (min-width:768px){
         width:30%;
@@ -22,44 +21,38 @@ const StyledCardMedia = styled(CardMedia)`
 
 const StyledDiv = styled.div`
     @media (min-width:768px){
-    display:flex;
-    flex-direction:row;
+        display:flex;
+        flex-direction:row;
     }
 `;
 
 const StyledCardContent = styled(CardContent)`
-@media (min-width:768px){
+    @media (min-width:768px){
         width:100%;
     }
 `;
 
-const CategoryTile = () => (
+const CategoryTile = ({ img, title, desc, link }) => (
     <Card >
         <StyledDiv>
             <StyledCardMedia
-                image={logo}
-                title="Contemplative Reptile"
+                image={img}
+                title={title}
             />
             <CardActionArea>
 
                 <StyledCardContent>
                     <Typography gutterBottom variant="h5" component="h2">
-                        Lizard
-          </Typography>
+                        {title}
+                    </Typography>
                     <Typography variant="body2" color="textSecondary" component="p">
-                        Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging
-                        across all continents except Antarctica
-          </Typography>
+                        {desc}
+                    </Typography>
                 </StyledCardContent>
             </CardActionArea>
         </StyledDiv>
         <CardActions>
-            <Button size="small" color="primary">
-                Share
-        </Button>
-            <Button size="small" color="primary">
-                Learn More
-        </Button>
+            <Button component={Link} to={link} size="small" color="primary">Czytaj dalej</Button>
         </CardActions>
     </Card>
 );
