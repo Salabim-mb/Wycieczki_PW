f420e01e3ea306f1ed68853f376c9ed32cffa9ad
Kacper <kucza06@wp.pl>
2021-02-18 22:24:55 +0100

    change CategoryTile to preview (delete after accepting pr)

--- src/root/App/App.jsx
+++ src/root/App/App.jsx
@@ -6,13 +6,16 @@ import { ThemeProvider } from 'styled-components';
 import theme from 'constants/theme';
 import CategoryTile from 'components/CategoryTile/CategoryTile';
 
+import logo from 'logo.svg'
+
 const queryClient = new QueryClient();
 
 const App = () => (
 	<ThemeProvider theme={theme}>
 		<QueryClientProvider client={queryClient}>
 			<BrowserRouter>
-				<CategoryTile />
+				<CategoryTile img={logo} title='dupa' desc='to tam troche dłuższe, to nw moze być docelowo ileś znaków z postu, założmy 30 i potem na koniec ..., albo jakieś oddzilen w kreatorze.' link='/blog/category/1' /> 
+				{/* to wyżej to do wywalenia jak zaakcapotwany bedzie pr */}
 				<Router />
 			</BrowserRouter>
 		</QueryClientProvider>
