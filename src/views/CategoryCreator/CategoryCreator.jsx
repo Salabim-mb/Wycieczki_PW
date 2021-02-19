55cf5d10e8447515af0d12a51e11e037df4a744e
Kacper <kucza06@wp.pl>
2021-02-19 09:02:13 +0100

    add Container and h2

--- src/views/CategoryCreator/CategoryCreator.jsx
+++ src/views/CategoryCreator/CategoryCreator.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { useFormik } from 'formik';
 import TextField from '@material-ui/core/TextField';
 import Input from '@material-ui/core/Input';
+import Container from '@material-ui/core/Container';
 
 
 const CategoryCreator = () => {
@@ -18,16 +19,19 @@ const CategoryCreator = () => {
 
 
     return (
-        <form onSubmit={formik.handleSubmit}>
-            <TextField id="title" label="Tytuł" type="text" value={formik.values.title} onChange={event => {
-                formik.setFieldValue('title', event.target.value);
-            }} />
-            <Input id="cover"
-                name="cover"
-                type="file" onChange={event => {
-                    formik.setFieldValue('cover', event.currentTarget.files[0]);
+        <Container>
+            <h2>Dodaj kategorię</h2>
+            <form onSubmit={formik.handleSubmit}>
+                <TextField id="title" label="Tytuł" type="text" value={formik.values.title} onChange={event => {
+                    formik.setFieldValue('title', event.target.value);
                 }} />
-        </form>
+                <Input id="cover"
+                    name="cover"
+                    type="file" onChange={event => {
+                        formik.setFieldValue('cover', event.currentTarget.files[0]);
+                    }} />
+            </form>
+        </Container>
     )
 }
 
