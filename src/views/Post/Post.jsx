52048d4bd2a707b1990823479bb71bf1e69914c5
Kacper <kucza06@wp.pl>
2021-02-23 20:00:32 +0100

    fix api and add error handling

--- src/views/Post/Post.jsx
+++ src/views/Post/Post.jsx
@@ -1,4 +1,5 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
+import { useQuery } from 'react-query'
 import { useParams } from 'react-router-dom'
 import paths from 'constants/api'
 import Container from '@material-ui/core/Container';
@@ -7,54 +8,53 @@ import Typography from '@material-ui/core/Typography';
 import CategoryTile from 'components/CategoryTile'
 
 const Post = () => {
-    const [categories, setCategories] = useState()
-    const [blog, setBlog] = useState()
+    // const [categories, setCategories] = useState()
+    // const [blog, setBlog] = useState()
     const params = useParams()
 
-    const getCategories = () => {
-        fetch(`${paths.BLOG_TOPICS}?blog=${params.blog}`, {
-            method: 'GET',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-        })
-            .then(response => response.json())
-            .then(data => {
-                console.log('Success:', data);
-                setCategories(data)
-            })
-            .catch((error) => {
-                console.error('Error:', error);
-            });
-    }
+    const getCategories = async () => {
+        const url = `${paths.BLOG_TOPICS}?blog=${params.blog}`;
 
-    const getBlog = () => {
-        fetch(`${paths.BLOG_TOPICS}${params.blog}`, {
-            method: 'GET',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-        })
-            .then(response => response.json())
-            .then(data => {
-                console.log('Success:', data);
-                setBlog(data)
-            })
-            .catch((error) => {
-                console.error('Error:', error);
-            });
-    }
+        const headers = {
+            'Content-Type': 'application/json',
+        };
 
-    useEffect(() => {
-        getCategories()
-        getBlog()
-    }, [])
+        const response = await fetch(url, { headers, method: 'GET' });
+
+        if (response.status !== 200) {
+            throw new Error('błąd');
+        }
+
+        return response.json();
+    };
+
+    const getBlog = async () => {
+        const url = `${paths.BLOG}${params.blog}/`;
+
+        const headers = {
+            'Content-Type': 'application/json',
+        };
+
+        const response = await fetch(url, { headers, method: 'GET' });
+
+        if (response.status !== 200) {
+            throw new Error('błąd');
+        }
+
+        return response.json();
+    };
+
+
+    const categories = useQuery('categories', getCategories)
+    const blog = useQuery('blog', getBlog)
 
     return (
         <Container>
-            <img src={blog?.image_url} alt={blog?.title} />
-            <Typography variant='h2'>{blog?.title}</Typography>
-            {categories?.map((category) => (
+            {blog.isError ? <div>Nie można wczytać blogów</div> : <>
+                <img src={blog?.data?.image_url} alt={blog?.data?.title} />
+                <Typography variant='h2'>{blog?.data?.title}</Typography>
+            </>}
+            {categories.isError ? <div>Nie można załadować kategorii</div> : categories.data?.map((category) => (
                 <CategoryTile key={category.id} title={category.title} cover={`https://wut-vtrips.herokuapp.com${category.cover_url}`} desc={category.description} link={`/${params.blog}/${category.id}`} />
             ))}
         </Container>
