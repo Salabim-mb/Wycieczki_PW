429c778e2fd84c12067413d8721b4f71508e7b31
Kacper <kucza06@wp.pl>
2021-02-23 20:19:51 +0100

    fix api again and use react-query

--- src/views/Category/Category.jsx
+++ src/views/Category/Category.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import { useQuery } from 'react-query'
 import { useParams } from "react-router-dom";
 import PostTile from 'components/PostTile'
@@ -9,58 +9,52 @@ import StyledContainer from './Category.css'
 
 
 const Category = () => {
-    const [category, setCategory] = useState([])
-    const [categoryErr, setCategoryErr] = useState([])
-    const [posts, setPosts] = useState([])
     const params = useParams();
 
 
-    const recivePosts = () => {
-        fetch(`${paths.BLOG_POSTS}?topic=${params.category}`, {
-            method: 'GET',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-        })
-            .then(response => response.json())
-            .then(data => {
-                setPosts(data)
-            })
-            .catch((error) => {
-                console.error('ErrorPOST:', error);
-            });
-    }
+    const getPosts = async () => {
+        const url = `${paths.BLOG_POSTS}?topic=${params.category}`;
 
-    const reciveCategory = () => {
-        fetch(`${paths.BLOG_TOPIC}${params.category}`, {
-            method: 'GET',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-        })
-            .then(response => response.json())
-            .then(data => {
-                setCategory(data)
-            })
-            .catch((error) => {
-                setCategoryErr(error)
-            });
-    }
-    const { isLoading } = useQuery('posts', () => recivePosts())
-    const { results } = posts
-    useEffect(() => {
-        reciveCategory()
-    }, [])
+        const headers = {
+            'Content-Type': 'application/json',
+        };
 
+        const response = await fetch(url, { headers, method: 'GET' });
+
+        if (response.status !== 200) {
+            throw new Error('błąd');
+        }
+
+        return response.json();
+    };
+
+    const getCategory = async () => {
+        const url = `${paths.BLOG_TOPIC}${params.category}/`;
+
+        const headers = {
+            'Content-Type': 'application/json',
+        };
+
+        const response = await fetch(url, { headers, method: 'GET' });
+
+        if (response.status !== 200) {
+            throw new Error('błąd');
+        }
+
+        return response.json();
+    };
+
+    const posts = useQuery('posts', getPosts)
+    const category = useQuery('category', getCategory)
 
     return (
         <StyledContainer>
-            {categoryErr ? <div>Nie udało się pobrać kategorii</div> : <>
-                <Image imgSrc={`${paths.PLAIN}${category?.cover_url}`} imgAlt={category?.title} />
-                <Typography variant="h2">{category?.title}</Typography>
+            {category.isError ? <div>Nie udało się pobrać kategorii</div> : <>
+                <Image imgSrc={`${paths.PLAIN}${category?.data?.cover_url}`} imgAlt={category?.data?.title} />
+                <Typography variant="h2">{category?.data?.title}</Typography>
             </>}
 
-            { isLoading ? <div> Ładowanie... </div> : results?.map((data) => (
+            { posts.isError ? <div> Nie udało się pobrać postu </div> : posts?.data?.results?.map((data) => (
                 <PostTile key={data.id} id={data.id} cover={data.cover} title={data.title} summary={data.summary} link={`/${params.category}/${data.id}`} />
             ))}
         </StyledContainer>
