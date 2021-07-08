dc8e05cf305d549a58d9164c8367646d6cb0cff2
adaxelx <adaxelx@o2.pl>
2021-07-08 23:53:59 +0200

    fix require to import

--- src/utils/aesCiphrator.js
+++ src/utils/aesCiphrator.js
@@ -1,30 +1,38 @@
-const aesjs = require('aes-js');
+import aesjs from 'aes-js';
 
-const encryptCookie = (data) => {
+const encryptCookie = data => {
 	try {
 		const stringKey = process.env.REACT_APP_COOKIE_ENCRYPTION_KEY;
-		const key = stringKey.split('').map((item) => item.charCodeAt(0));
-		const dataBytes = aesjs.utils.utf8.toBytes( JSON.stringify(data) );
+		const key = stringKey.split('').map(item => item.charCodeAt(0));
+		const dataBytes = aesjs.utils.utf8.toBytes(JSON.stringify(data));
 		// eslint-disable-next-line new-cap
-		const encryptedBytes = (new aesjs.ModeOfOperation.ctr(new Uint8Array(key), new aesjs.Counter(5))).encrypt(dataBytes);
+		const encryptedBytes = new aesjs.ModeOfOperation.ctr(
+			new Uint8Array(key),
+			new aesjs.Counter(5),
+		).encrypt(dataBytes);
 		return aesjs.utils.hex.fromBytes(encryptedBytes);
-	} catch(e) {
-		alert("Wystąpił błąd przy szyfrowaniu Twoich plików Cookie. Twoja sesja nie będzie w pełni bezpieczna!");
+	} catch (e) {
+		alert(
+			'Wystąpił błąd przy szyfrowaniu Twoich plików Cookie. Twoja sesja nie będzie w pełni bezpieczna!',
+		);
 		return data;
 	}
 };
 
-const decryptCookie = (enc) => {
+const decryptCookie = enc => {
 	if (enc) {
 		try {
 			const stringKey = process.env.REACT_APP_COOKIE_ENCRYPTION_KEY;
 			let key = stringKey.split('');
-			key = key.map((item) => item.charCodeAt(0));
+			key = key.map(item => item.charCodeAt(0));
 			const encryptedBytes = aesjs.utils.hex.toBytes(enc);
 			// eslint-disable-next-line new-cap
-			const decryptedBytes = (new aesjs.ModeOfOperation.ctr(new Uint8Array(key), new aesjs.Counter(5))).decrypt(encryptedBytes);
-			return JSON.parse( aesjs.utils.utf8.fromBytes(decryptedBytes) );
-		} catch(e) {
+			const decryptedBytes = new aesjs.ModeOfOperation.ctr(
+				new Uint8Array(key),
+				new aesjs.Counter(5),
+			).decrypt(encryptedBytes);
+			return JSON.parse(aesjs.utils.utf8.fromBytes(decryptedBytes));
+		} catch (e) {
 			// alert("Nie możemy odczytać Twoich danych. Skontaktuj się z administratorem aplikacji.");
 			return undefined;
 		}
@@ -33,7 +41,4 @@ const decryptCookie = (enc) => {
 	}
 };
 
-export {
-	decryptCookie,
-	encryptCookie
-}
+export { decryptCookie, encryptCookie };
